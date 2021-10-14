7f38621eab5542520ed9248a1a5256629c93ee8f
rvanooyen <robert.vanooyen@gmail.com>
2021-10-13 20:52:47 -0500

    in progress

--- index.js
+++ index.js
@@ -6,9 +6,86 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
 
-const employee = new Employee;
+const employeeArr = [];
 
-const promptEmployee = () => {
+const promptManager = () => {
+    const manager = new Manager;
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter the manager's name. (Required)",
+            validate: nameInput => {
+                if (nameInput) {                    
+                    return true;
+                } else {
+                    console.log("Please enter the manager's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the manager's id number. (Required)",
+            validate: idInput => {
+                if (idInput) {                    
+                    return true;
+                } else {
+                    console.log("Please enter the manager's id number!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email address. (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's email address!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please select the employee's role.",
+            choices: ['Manager'],
+            default: 'Manager'            
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the employee's office number. (Required)",
+            validate: officeInput => {
+                if (officeInput) {
+                    employeeArr.push(this);
+                    console.log(this);                    
+                    return true;
+                } else {
+                    console.log("Please enter the employee's office number!");
+                    return false;
+                }
+            }
+        }
+    ]);    
+};
+
+const promptEmployee = (employeeData) => {
+    const employee = new Employee(employeeData.name, employeeData.id, employeeData.email, employeeData.role);
+    
+    if (!employeeData) {
+        employeeData = [];
+    }
+    
+    employeeArr.push(employee);   
+
+    console.log(employeeData);
     return inquirer.prompt([
         {
             type: 'input',
@@ -53,120 +130,100 @@ const promptEmployee = () => {
             type: 'list',
             name: 'role',
             message: "Please select the employee's role.",
-            choices: ['Manager', 'Engineer', 'Intern']            
+            choices: ['Engineer', 'Intern']                     
         }
-    ]);    
-};
-
-const promptRole = (employeeData) => {
-    const employee = new Employee(employeeData.name, employeeData.id, employeeData.email, employeeData.role);
-
-    if (!employeeData) {
-        employeeData = [];
+        ]).then(employeeData => {
+            employee.setRole(employeeData.role);
+            console.log(`
+            =====================
+            Add role specific data
+            =====================
+            `);
+            switch (employee.role) {                
+                case 'Engineer':
+                    const engineer = new Engineer(employeeData.name, employeeData.id, employeeData.email, employeeData.role);
+        
+                    engineer.name = engineer.getName();
+                    engineer.id = engineer.getId();
+                    engineer.email = engineer.getEmail();
+                    engineer.role = engineer.getRole();
+        
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'githubName',
+                            message: "Please enter the employee's github name. (Required)",
+                            validate: githubInput => {
+                                if (githubInput) {                                                                    
+                                    engineer.githubName = employeeData.githubName;
+                                    console.log(this);
+                                    console.log(engineer);
+                                    employeeArr.push(engineer);
+                                    return true;
+                                } else {
+                                    console.log("Please enter the employee's github name!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'confirm',
+                            name: 'confirmNewEmployee',
+                            message: 'Would you like to add another employee?',
+                            default: false
+                        }
+                    ]).then(employeeData => {        
+                        if (employeeData.confirmNewEmployee) {
+                            return promptEmployee();
+                        } else {
+                            return employeeData;
+                        }        
+                    });                    
+                    break;            
+                case 'Intern':
+                    const intern = new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.role);
+        
+                    intern.name = intern.getName();
+                    intern.id = intern.getId();
+                    intern.email = intern.getEmail();
+                    intern.role = intern.getRole();
+        
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'school',
+                            message: "Please enter the employee's school name. (Required)",
+                            validate: schoolInput => {
+                                if (schoolInput) {
+                                    intern.school = employeeData.school;
+                                    employeeArr.push(intern);
+                                    return true;
+                                } else {
+                                    console.log("Please enter the employee's school name!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'confirm',
+                            name: 'confirmNewEmployee',
+                            message: 'Would you like to add another employee?',
+                            default: false
+                        }
+                    ]).then(employeeData => {        
+                        if (employeeData.confirmNewEmployee) {
+                            return promptEmployee();
+                        } else {
+                            return employeeData;
+                        }        
+                    });                    
+                    break;                                    
+            }
+        });
     }
 
-    employee.name = employee.getName();
-    employee.id = employee.getId();
-    employee.email = employee.getEmail();
-    employee.role = employee.getRole();
-
-    console.log(employeeData);
-    console.log(employee);
-
-    console.log(`
-    =====================
-    Add role specific data
-    =====================
-    `);
-    switch (employee.role) {
-        case 'Manager':
-            const manager = new Manager(employee.name, employee.id, employee.email, employee.role);
-
-            manager.name = manager.getName();
-            manager.id = manager.getId();
-            manager.email = manager.getEmail();
-            manager.role = manager.getRole();           
-
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'officeNumber',
-                    message: "Please enter the employee's office number. (Required)",
-                    validate: officeInput => {
-                        if (officeInput) {
-                            return true;
-                        } else {
-                            console.log("Please enter the employee's office number!");
-                            return false;
-                        }
-                }
-            }]);
-        case 'Engineer' :
-            const engineer = new Engineer;
-
-            engineer.name = engineer.getName();
-            engineer.id = engineer.getId();
-            engineer.email = engineer.getEmail();
-            engineer.role = engineer.getRole();
-
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'githubName',
-                    message: "Please enter the employee's github name. (Required)",
-                    validate: githubInput => {
-                        if (githubInput) {
-                            return true;
-                        } else {
-                            console.log("Please enter the employee's github name!");
-                            return false;
-                        }
-                }
-            }]);            
-        case 'Intern' :
-            const intern = new Intern;
-
-            intern.name = intern.getName();
-            intern.id = intern.getId();
-            intern.email = intern.getEmail();
-            intern.role = intern.getRole();
-
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'school',
-                    message: "Please enter the employee's school name. (Required)",
-                    validate: schoolInput => {
-                        if (schoolInput) {
-                            return true;
-                        } else {
-                            console.log("Please enter the employee's school name!");
-                            return false;
-                        }
-                }
-            }]);
-    }        
-};
-
-const promptNewEmployee = (employeeData) => {
-    return inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'confirmNewEmployee',
-            message: 'Would you like to add another employee?',
-            default: false
-        }]).then(employeeData => {        
-            if (employeeData.confirmNewEmployee) {
-                return promptEmployee();
-            } else {
-                return employeeData;
-            }        
-        });
-};
-
-promptEmployee()
-    .then(promptRole)
-    // .then(promptNewEmployee(employeeData))
+promptManager()
+    .then(promptEmployee)
     // .then(writeFileResponse => {
     //     console.log(writeFileResponse);
     //     return copyFile();
